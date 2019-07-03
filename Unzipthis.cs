08943de4ea3650980526bbd39d97123449bf8781
Frank <fboucher@outlook.com>
2019-07-03 10:48:51 -0400

    temp

--- Unzipthis.cs
+++ Unzipthis.cs
@@ -4,7 +4,6 @@ using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -14,9 +13,10 @@ namespace AzUnzipEverything
     public static class Unzipthis
     {
         [FunctionName("Unzipthis")]
-        public static async Task Run([BlobTrigger("input-files/{name}", Connection = "cloud5mins_storage")]Stream myBlob, string name, ILogger log)
+        public static async Task Run([BlobTrigger("input-files/{name}", Connection = "cloud5mins_storage")]CloudBlockBlob myBlob, string name, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            //log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name}");
 
             string destinationStorage = Environment.GetEnvironmentVariable("destinationStorage");
             string destinationContainer = Environment.GetEnvironmentVariable("destinationContainer");
@@ -24,26 +24,34 @@ namespace AzUnzipEverything
             try{
                 if(name.Split('.').Last().ToLower() == "zip"){
 
-                    ZipArchive archive = new ZipArchive(myBlob);
+                    //ZipArchive archive = new ZipArchive(myBlob, ZipArchiveMode.Read,true);
 
+                    
                     CloudStorageAccount storageAccount = CloudStorageAccount.Parse(destinationStorage);
                     CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                     CloudBlobContainer container = blobClient.GetContainerReference(destinationContainer);
+                    using( var streamBlob = new MemoryStream()){
 
-                    foreach (ZipArchiveEntry entry in archive.Entries)
-                    {
-                        log.LogInformation($"Now processing {entry.FullName}");
+                        await myBlob.DownloadToStreamAsync(streamBlob);
 
-                        CloudBlockBlob blockBlob = container.GetBlockBlobReference(entry.Name);
-                        using (var fileStream = entry.Open())
+                        using (var archive = new ZipArchive(streamBlob, ZipArchiveMode.Read,true))
                         {
-                            await blockBlob.UploadFromStreamAsync(fileStream);
+                            foreach (ZipArchiveEntry entry in archive.Entries)
+                            {
+                                log.LogInformation($"Now processing {entry.FullName}");
+
+                                CloudBlockBlob blockBlob = container.GetBlockBlobReference(entry.Name);
+                                using (var fileStream = entry.Open())
+                                {
+                                    await blockBlob.UploadFromStreamAsync(fileStream);
+                                }
+                            }
                         }
                     }
                 }
             }
             catch(Exception ex){
-                log.LogInformation($"Error! Something went wrong: {ex.Message}");
+                log.LogInformation($"Error! Something went wrong: {ex.Message} INNER: {ex.InnerException}");
 
             }            
         }
