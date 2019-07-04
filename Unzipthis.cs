9207e08ccfbf61a6cf7c6cee411afe1f3ab42aff
Frank Boucher <2404846+FBoucher@users.noreply.github.com>
2019-07-03 20:31:56 -0400

    Fixing #3 (#4)
    * Change Bindings and Replace invalid characters in the filename

--- Unzipthis.cs
+++ Unzipthis.cs
@@ -2,9 +2,9 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -14,9 +14,9 @@ namespace AzUnzipEverything
     public static class Unzipthis
     {
         [FunctionName("Unzipthis")]
-        public static async Task Run([BlobTrigger("input-files/{name}", Connection = "cloud5mins_storage")]Stream myBlob, string name, ILogger log)
+        public static async Task Run([BlobTrigger("input-files/{name}", Connection = "cloud5mins_storage")]CloudBlockBlob myBlob, string name, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name}");
 
             string destinationStorage = Environment.GetEnvironmentVariable("destinationStorage");
             string destinationContainer = Environment.GetEnvironmentVariable("destinationContainer");
@@ -24,20 +24,29 @@ namespace AzUnzipEverything
             try{
                 if(name.Split('.').Last().ToLower() == "zip"){
 
-                    ZipArchive archive = new ZipArchive(myBlob);
-
                     CloudStorageAccount storageAccount = CloudStorageAccount.Parse(destinationStorage);
                     CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                     CloudBlobContainer container = blobClient.GetContainerReference(destinationContainer);
+                    
+                    using(MemoryStream blobMemStream = new MemoryStream()){
 
-                    foreach (ZipArchiveEntry entry in archive.Entries)
-                    {
-                        log.LogInformation($"Now processing {entry.FullName}");
+                        await myBlob.DownloadToStreamAsync(blobMemStream);
 
-                        CloudBlockBlob blockBlob = container.GetBlockBlobReference(entry.Name);
-                        using (var fileStream = entry.Open())
+                        using(ZipArchive archive = new ZipArchive(blobMemStream))
                         {
-                            await blockBlob.UploadFromStreamAsync(fileStream);
+                            foreach (ZipArchiveEntry entry in archive.Entries)
+                            {
+                                log.LogInformation($"Now processing {entry.FullName}");
+
+                                //Replace all NO digits, letters, or "-" by a "-" Azure storage is specific on valid characters
+                                string valideName = Regex.Replace(entry.Name,@"[^a-zA-Z0-9\-]","-");
+
+                                CloudBlockBlob blockBlob = container.GetBlockBlobReference(valideName);
+                                using (var fileStream = entry.Open())
+                                {
+                                    await blockBlob.UploadFromStreamAsync(fileStream);
+                                }
+                            }
                         }
                     }
                 }
